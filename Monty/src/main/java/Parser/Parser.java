df6bf6a6305fcb844bd4fd75950288fdd1a83882
ikcilrep <ikcilrep@retupmok.home>
2018-07-10 11:15:54 +0200

    Began work on identification some expressions and statements.

---
+++ Monty/src/main/java/Parser/Parser.java
@@ -0,0 +1,47 @@
+package Parser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import AST.Node;
+import Lexer.MontyToken;
+import Lexer.TokenTypes;
+
+public class Parser {
+	private static final String DATA_TYPES = "INTEGER_LITERAL|BOOLEAN_LITERAL|STRING_LITERAL|FLOAT_LITERAL";
+	private static final String DATA_TYPES_KEYWORDS = "INTEGER_KEYWORD|BOOLEAN_KEYWORD|STRING_KEYWORD|FLOAT_KEYWORD";
+	private static final String EXPRESSION = "(((IDENTIFIER|" + DATA_TYPES + ") )+(OPERATOR )*)+";
+	// Regular Expressions
+	private static final Pattern VARIABLE_DECLARATION_REGEX = Pattern
+			.compile("^VAR_KEYWORD (" + DATA_TYPES_KEYWORDS + ") " + EXPRESSION + "$");
+	private static final Pattern EXPRESSION_REGEX = Pattern.compile("^" + EXPRESSION + "$");
+	private static final Pattern PRINT_STATEMENT_REGEX = Pattern.compile("^PRINT_KEYWORD "+EXPRESSION+"$");
+	//
+	public static String getText(List<MontyToken> tokens) {
+		var text = new StringBuilder();
+		for (MontyToken token : tokens)
+			text.append(token.getType() + " ");
+		return text.toString();
+	}
+
+	public static Node parse(List<MontyToken> tokens) {
+		var tokensBeforeSemicolon = new ArrayList<MontyToken>();
+		for (MontyToken token : tokens) {
+			if (token.getType().equals(TokenTypes.SEMICOLON)) {
+				String text = getText(tokensBeforeSemicolon);
+				System.out.println("\n"+text);
+				if (EXPRESSION_REGEX.matcher(text).matches())
+					System.out.println("EXPRESSION!");
+				else if (VARIABLE_DECLARATION_REGEX.matcher(text).matches())
+					System.out.println("VARIABLE DECLARATION!");
+				else if (PRINT_STATEMENT_REGEX.matcher(text).matches())
+					System.out.println("PRINT STATEMENT!");
+				tokensBeforeSemicolon.clear();
+			} else
+				tokensBeforeSemicolon.add(token);
+		}
+		return null;
+
+	}
+}
