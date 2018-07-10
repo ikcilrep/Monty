b4fd11e6087619d0dcfe5d70dd0ce197518510c1
ikcilrep <ikcilrep@retupmok.home>
2018-07-10 11:16:31 +0200

    Variable declaration parser.

---
+++ Monty/src/main/java/Parser/VariableDeclarationParser.java
@@ -0,0 +1,29 @@
+package Parser;
+
+import java.util.List;
+
+import AST.VariableDeclarationNode;
+import Lexer.MontyToken;
+
+public class VariableDeclarationParser {
+	public static VariableDeclarationNode parse(List<MontyToken> declaration) {
+		var dataType = (DataTypes) null;
+		switch (declaration.get(0).getType()) {
+		case INTEGER_KEYWORD:
+			dataType = DataTypes.INTEGER;
+			break;
+		case FLOAT_KEYWORD:
+			dataType = DataTypes.FLOAT;
+			break;
+		case BOOLEAN_KEYWORD:
+			dataType = DataTypes.BOOLEAN;
+			break;
+		case STRING_KEYWORD:
+			dataType = DataTypes.STRING;
+			break;
+		default:
+			break;
+		}
+		return new VariableDeclarationNode(declaration.get(1).getText(), dataType);
+	}
+}
