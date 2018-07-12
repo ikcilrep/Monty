7e49fc388df2a0463cd621199779298c3114ac38
ikcilrep <ikcilrep@retupmok.home>
2018-07-12 22:00:13 +0200

    Prototype of block of code.

---
+++ Monty/src/main/java/ast/Block.java
@@ -0,0 +1,24 @@
+package ast;
+
+import java.util.ArrayList;
+
+public class Block extends Node {
+	private ArrayList<Node> children = new ArrayList<>();
+	private Node parent;
+
+	public Block(Node parent) {
+		this.parent = parent;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public ArrayList<Node> getChildren() {
+		return children;
+	}
+
+	public void addChildren(Node child) {
+		children.add(child);
+	}
+}
