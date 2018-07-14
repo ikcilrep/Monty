5785dca1343602785d0dad455cb416461be9f541
ikcilrep <ikcilrep@retupmok.home>
2018-07-14 11:26:54 +0200

    Function which identificates variable declaration was added. Some exception messages was changed.

--- Monty/src/main/java/parser/Identificator.java
+++ Monty/src/main/java/parser/Identificator.java
@@ -8,6 +8,7 @@ import lexer.TokenTypes;
 public abstract class Identificator {
 	public static boolean isExpression(List<MontyToken> tokens) {
 		MontyToken last = null;
+		int i = 0;
 		for (MontyToken token : tokens) {
 			switch (token.getType()) {
 			case OPERATOR:
@@ -22,16 +23,17 @@ public abstract class Identificator {
 				break;
 			case BRACKET:
 				if (last.getType().equals(TokenTypes.COMMA) || last.equals(null))
-					new MontyException("Unexpected comma before bracket:\t" + Tokens.getText(tokens));
+					new MontyException("Unexpected comma before bracket:\t" + Tokens.getText(tokens.subList(0, i+1)));
 				break;
 			case COMMA:
 				if ((last.getType().equals(TokenTypes.BRACKET) && last.getText().equals("(")) || last.equals(null))
-					new MontyException("Unexpected bracket before comma:\t" + Tokens.getText(tokens));
+					new MontyException("Unexpected bracket before comma:\t" + Tokens.getText(tokens.subList(0, i+1)));
 				break;
 			default:
 				return false;
 			}
 			last = token;
+			i++;
 		}
 		return true;
 
@@ -39,12 +41,15 @@ public abstract class Identificator {
 
 	public static boolean isPrintStatement(List<MontyToken> tokens) {
 		var isFirstTokenPrintKeyword = tokens.get(0).getType().equals(TokenTypes.PRINT_KEYWORD);
-		if ((tokens.size() == 1 && isFirstTokenPrintKeyword)
-				|| (tokens.size() > 1 && isFirstTokenPrintKeyword && !isExpression(tokens.subList(1, tokens.size()))))
-			new MontyException("Expected expression after print keyword:\t" + Tokens.getText(tokens));
 		if (!isFirstTokenPrintKeyword)
 			return false;
-
+		if (tokens.size() == 1 && isFirstTokenPrintKeyword)
+			new MontyException("Expected expression after print keyword:\t" + Tokens.getText(tokens));
+		if (tokens.size() > 1) {
+			var expression = tokens.subList(1, tokens.size());
+			if (isFirstTokenPrintKeyword && !isExpression(expression))
+				new MontyException("Wrong expression after print keyword:\t" + Tokens.getText(expression));
+		}
 		return true;
 	}
 
@@ -55,6 +60,25 @@ public abstract class Identificator {
 			return false;
 		if (isFirstTokenReturnKeyword && tokens.size() > 1 && !isExpression(tokens.subList(1, tokens.size())))
 			new MontyException("Wrong expression after return keyword:\t" + Tokens.getText(tokens));
+		return true;
+	}
+
+	public static boolean isVariableDeclaration(List<MontyToken> tokens) {
+		var isFirstTokenVarKeyword = tokens.get(0).getType().equals(TokenTypes.VAR_KEYWORD);
+		var isSecondTokenDataTypeKeyword = tokens.size() > 1
+				&& (tokens.get(1).getType().equals(TokenTypes.INTEGER_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.FLOAT_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.STRING_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.BOOLEAN_KEYWORD));
+		if (!isFirstTokenVarKeyword)
+			return false;
+		if (!isSecondTokenDataTypeKeyword)
+			new MontyException("Expected data type declaration after var keyword:\t" + Tokens.getText(tokens));
+		if (isSecondTokenDataTypeKeyword && tokens.size() == 2)
+			new MontyException("Expected expression after data type declaration:\t" + Tokens.getText(tokens));
+		var expression = tokens.subList(2, tokens.size());
+		if (!isExpression(expression))
+			new MontyException("Wrong expression after data type declaration:\t" + Tokens.getText(expression));
 		return true;
 	}
 }
