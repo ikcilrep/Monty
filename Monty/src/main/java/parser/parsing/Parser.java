acd728aaa05dc96f056661898162d097ddf71144
ikcilrep <ikcilrep@retupmok.home>
2018-07-14 20:09:51 +0200

    Began work on real parsing.

--- Monty/src/main/java/parser/parsing/Parser.java
+++ Monty/src/main/java/parser/parsing/Parser.java
@@ -2,25 +2,34 @@ package parser.parsing;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ast.Block;
 import ast.Node;
 import lexer.MontyToken;
 import lexer.TokenTypes;
+import parser.AdderToBlock;
 import parser.Identificator;
 
 public class Parser {
 
 	public static Node parse(List<MontyToken> tokens) {
 		var tokensBeforeSemicolon = new ArrayList<MontyToken>();
+		var block = new Block(null);
 		for (MontyToken token : tokens) {
 			if (token.getType().equals(TokenTypes.SEMICOLON)) {
-				if (Identificator.isExpression(tokensBeforeSemicolon))
+				if (Identificator.isExpression(tokensBeforeSemicolon)) {
 					System.out.println("EXPRESSION!");
-				else if (Identificator.isPrintStatement(tokensBeforeSemicolon))
+					AdderToBlock.addExpression(block, tokensBeforeSemicolon);
+				} else if (Identificator.isPrintStatement(tokensBeforeSemicolon)) {
 					System.out.println("PRINT STATEMENT!");
-				else if (Identificator.isVariableDeclaration(tokensBeforeSemicolon))
+					AdderToBlock.addPrintStatement(block, tokensBeforeSemicolon);
+				} else if (Identificator.isVariableDeclaration(tokensBeforeSemicolon)) {
 					System.out.println("VARIABLE DECLARATION!");
-				else if (Identificator.isReturnStatement(tokensBeforeSemicolon))
+					AdderToBlock.addVariableDeclaration(block, tokens);
+				} else if (Identificator.isReturnStatement(tokensBeforeSemicolon)) {
 					System.out.println("RETURN STATEMENT!");
+					AdderToBlock.addReturnStatement(block, tokensBeforeSemicolon);
+				}
 				tokensBeforeSemicolon.clear();
 			} else
 				tokensBeforeSemicolon.add(token);
