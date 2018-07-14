1fe883926d0f2e61882de7adec363c0a70560a5f
ikcilrep <ikcilrep@retupmok.home>
2018-07-14 23:31:09 +0200

    Function which identificates function declaration was added.

--- Monty/src/main/java/parser/Identificator.java
+++ Monty/src/main/java/parser/Identificator.java
@@ -44,11 +44,11 @@ public abstract class Identificator {
 		if (!isFirstTokenPrintKeyword)
 			return false;
 		if (tokens.size() == 1 && isFirstTokenPrintKeyword)
-			new MontyException("Expected expression after print keyword:\t" + Tokens.getText(tokens));
+			new MontyException("Expected expression after \"print\" keyword:\t" + Tokens.getText(tokens));
 		if (tokens.size() > 1) {
 			var expression = tokens.subList(1, tokens.size());
 			if (isFirstTokenPrintKeyword && !isExpression(expression))
-				new MontyException("Wrong expression after print keyword:\t" + Tokens.getText(expression));
+				new MontyException("Wrong expression after \"print\" keyword:\t" + Tokens.getText(expression));
 		}
 		return true;
 	}
@@ -73,7 +73,7 @@ public abstract class Identificator {
 		if (!isFirstTokenVarKeyword)
 			return false;
 		if (!isSecondTokenDataTypeKeyword)
-			new MontyException("Expected data type declaration after var keyword:\t" + Tokens.getText(tokens));
+			new MontyException("Expected data type declaration after \"var\" keyword:\t" + Tokens.getText(tokens));
 		if (isSecondTokenDataTypeKeyword && tokens.size() == 2)
 			new MontyException("Expected expression after data type declaration:\t" + Tokens.getText(tokens));
 		var expression = tokens.subList(2, tokens.size());
@@ -82,6 +82,61 @@ public abstract class Identificator {
 		if (!tokens.get(2).getType().equals(TokenTypes.IDENTIFIER))
 			new MontyException("Expected identifier after data type declaration:\t" + Tokens.getText(expression));
 
+		return true;
+	}
+
+	public static boolean isFunctionDeclaration(List<MontyToken> tokens) {
+		var isFirstTokenFuncKeyword = tokens.get(0).getType().equals(TokenTypes.FUNC_KEYWORD);
+		var isSecondTokenDataTypeKeyword = tokens.size() > 1
+				&& (tokens.get(1).getType().equals(TokenTypes.INTEGER_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.FLOAT_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.STRING_KEYWORD)
+						|| tokens.get(1).getType().equals(TokenTypes.BOOLEAN_KEYWORD));
+		var isThirdTokenIdentifier = tokens.size() > 2 && tokens.get(2).getType().equals(TokenTypes.IDENTIFIER);
+		if (!isFirstTokenFuncKeyword)
+			return false;
+		if (!isSecondTokenDataTypeKeyword)
+			new MontyException("Expected data type declaration after \"func\" keyword:\t"
+					+ Tokens.getText(tokens.subList(1, tokens.size())));
+		if (!isThirdTokenIdentifier)
+			new MontyException("Expected identifier after data type declaration keyword:\t"
+					+ Tokens.getText(tokens.subList(2, tokens.size())));
+		var last = (TokenTypes) null;
+		var isLastTokenDataTypeDeclaration = false;
+		for (int i = 3; i < tokens.size(); i++) {
+			var t = tokens.get(i);
+			switch (t.getType()) {
+			case INTEGER_KEYWORD:
+			case FLOAT_KEYWORD:
+			case STRING_KEYWORD:
+			case BOOLEAN_KEYWORD:
+				isLastTokenDataTypeDeclaration = true;
+				break;
+			case IDENTIFIER:
+				if (!isLastTokenDataTypeDeclaration)
+					new MontyException("Expected data type declaration before identifer:\t"
+							+ Tokens.getText(tokens.subList(i, tokens.size())));
+				isLastTokenDataTypeDeclaration = false;
+				break;
+			case COMMA:
+				if (!(last.equals(TokenTypes.IDENTIFIER) && i + 1 < tokens.size()))
+					new MontyException("Unexpected comma:\t" + Tokens.getText(tokens.subList(i, tokens.size())));
+				isLastTokenDataTypeDeclaration = false;
+				break;
+			default:
+				new MontyException("Unexpected token:\t" + t.getText());
+			}
+			last = t.getType();
+		}
+		return true;
+
+	}
+	
+	public static boolean isEndKeyword(List<MontyToken> tokens) {
+		if (!tokens.get(0).getType().equals(TokenTypes.END_KEYWORD))
+			return false;
+		if (tokens.size() > 1)
+			new MontyException("Nothing expected after end keyword:\t" + Tokens.getText(tokens));
 		return true;
 	}
 }
