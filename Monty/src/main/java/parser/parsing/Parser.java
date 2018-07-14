51da0f33363f1302967637f535e99ef329138a3b
ikcilrep <ikcilrep@retupmok.home>
2018-07-14 23:32:15 +0200

    Parser function declaration.

--- Monty/src/main/java/parser/parsing/Parser.java
+++ Monty/src/main/java/parser/parsing/Parser.java
@@ -9,6 +9,7 @@ import lexer.MontyToken;
 import lexer.TokenTypes;
 import parser.AdderToBlock;
 import parser.Identificator;
+import parser.MontyException;
 
 public class Parser {
 
@@ -17,6 +18,8 @@ public class Parser {
 		var block = new Block(null);
 		for (MontyToken token : tokens) {
 			if (token.getType().equals(TokenTypes.SEMICOLON)) {
+				if (tokensBeforeSemicolon.size() == 0)
+					continue;
 				if (Identificator.isExpression(tokensBeforeSemicolon)) {
 					System.out.println("EXPRESSION!");
 					AdderToBlock.addExpression(block, tokensBeforeSemicolon);
@@ -25,10 +28,18 @@ public class Parser {
 					AdderToBlock.addPrintStatement(block, tokensBeforeSemicolon);
 				} else if (Identificator.isVariableDeclaration(tokensBeforeSemicolon)) {
 					System.out.println("VARIABLE DECLARATION!");
-					AdderToBlock.addVariableDeclaration(block, tokens);
+					AdderToBlock.addVariableDeclaration(block, tokensBeforeSemicolon);
 				} else if (Identificator.isReturnStatement(tokensBeforeSemicolon)) {
 					System.out.println("RETURN STATEMENT!");
 					AdderToBlock.addReturnStatement(block, tokensBeforeSemicolon);
+				} else if (Identificator.isFunctionDeclaration(tokensBeforeSemicolon)) {
+					System.out.println("FUNCTION DECLARTION!");
+					block = AdderToBlock.addFunctionDeclaration(block, tokensBeforeSemicolon).getBody();
+				} else if (Identificator.isEndKeyword(tokensBeforeSemicolon)) {
+					var parent = block.getParent();
+					if (parent == null)
+						new MontyException("Nothing to end!");
+					block = block.getParent();
 				}
 				tokensBeforeSemicolon.clear();
 			} else
