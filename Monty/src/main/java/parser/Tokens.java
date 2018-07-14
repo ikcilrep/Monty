b01766bf43fd34ff4cd99df38965ec5d24c1269b
ikcilrep <ikcilrep@retupmok.home>
2018-07-14 20:10:21 +0200

    Function getDataType was added.

--- Monty/src/main/java/parser/Tokens.java
+++ Monty/src/main/java/parser/Tokens.java
@@ -3,6 +3,7 @@ package parser;
 import java.util.List;
 
 import lexer.MontyToken;
+import lexer.TokenTypes;
 
 public class Tokens {
 	public static String getText(List<MontyToken> tokens) {
@@ -12,5 +13,24 @@ public class Tokens {
 			result.append(' ');
 		}
 		return result.toString();
+	}
+
+	public static DataTypes getDataType(TokenTypes type) {
+		switch (type) {
+		case INTEGER_LITERAL:
+		case INTEGER_KEYWORD:
+			return DataTypes.INTEGER;
+		case FLOAT_LITERAL:
+		case FLOAT_KEYWORD:
+			return DataTypes.FLOAT;
+		case BOOLEAN_LITERAL:
+		case BOOLEAN_KEYWORD:
+			return DataTypes.BOOLEAN;
+		case STRING_LITERAL:
+		case STRING_KEYWORD:
+			return DataTypes.STRING;
+		default:
+			return null;
+		}
 	}
 }
