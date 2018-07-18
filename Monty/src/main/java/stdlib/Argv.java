dfea91c560ef2440d5cad9b0b81612eaae6450de
Butsgamer <Butsgamer@Butgamer.home>
2018-07-18 07:55:50 +0200

    Std function which returns argv.

---
+++ Monty/src/main/java/stdlib/Argv.java
@@ -0,0 +1,31 @@
+package stdlib;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+import ast.Block;
+import ast.declarations.FunctionDeclarationNode;
+import ast.declarations.VariableDeclarationNode;
+import ast.expressions.OperationNode;
+import monty.Main;
+import parser.DataTypes;
+import parser.MontyException;
+
+public class Argv extends FunctionDeclarationNode {
+
+	public Argv() {
+		super("argv", DataTypes.STRING);
+		setBody(new Block(null));
+		addParameter(new VariableDeclarationNode("index", DataTypes.INTEGER));
+	}
+
+	@Override
+	public Object call(ArrayList<OperationNode> arguments) {
+		setArguments(arguments);
+		var index= ((BigInteger) getBody().getVariableByName("index").getValue()).intValue();
+		if (index >= Main.argv.length)
+			new MontyException("Index "+index+" is too large for length "+Main.argv.length+".");
+		return Main.argv[index];
+	}
+
+}
