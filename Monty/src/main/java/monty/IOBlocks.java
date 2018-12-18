9f1e39b28fb9c4809669b0ce84225e1eb3db6669
frajerzycki <frajerzycki@protonmail.com>
2018-12-18 21:25:52 +0100

    Some parts of code was moved from Main.java to IOBlocks.java.

---
+++ Monty/src/main/java/monty/IOBlocks.java
@@ -0,0 +1,82 @@
+package monty;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import ast.Block;
+import lexer.LexerConfig;
+import lexer.MontyToken;
+import parser.MontyException;
+import parser.parsing.Parser;
+
+public class IOBlocks {
+	public static Block readBlock(int from, String path) {
+		Importing.setLibraries(from);
+		if (path.endsWith(".mt"))
+			return readBlockFromFile(path);
+		else if (path.endsWith(".mtc"))
+			return readCompiledBlockFromFile(path);
+		new MontyException("Wrong file extensions");
+		return null;
+	}
+	
+	private static Block readBlockFromFile(String path) {
+		var lb = LexerConfig.getLexer(FileIO.readFile(path));
+		List<MontyToken> tokens = lb.getAllTokens();
+		var block = Parser.parse(tokens);
+		block.addFunction(new sml.data.returning.Nothing());
+		return block;
+	}
+	
+	private static Block readCompiledBlockFromFile(String path) {
+		return readBlockFromGZIP(readGZIPBlock(path), path);
+	}
+	private static GZIPInputStream readGZIPBlock(String path) {
+		FileInputStream fis = null;
+		GZIPInputStream gis = null;
+		try {
+			fis = new FileInputStream(path);
+			gis = new GZIPInputStream(fis);
+		} catch (IOException e) {
+			new MontyException("File not found:\t" + path);
+		}
+		return gis;
+	}
+	
+	private static Block readBlockFromGZIP(GZIPInputStream gis, String path) {
+		ObjectInputStream ois;
+		Block block = null;
+		try {
+			ois = new ObjectInputStream(gis);
+			var obj = ois.readObject();
+			if (!(obj instanceof Block))
+				new MontyException("This isn't file with program or library:\t" + path);
+			block = (Block) obj;
+			ois.close();
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+			new MontyException("Wrong file format");
+		}
+		return block;
+	}
+	
+	public static void compileAndWriteBlock(Block block, String outputPath) {
+		try {
+			FileOutputStream fos = new FileOutputStream(outputPath);
+			GZIPOutputStream gos = new GZIPOutputStream(fos);
+			ObjectOutputStream oos = new ObjectOutputStream(gos);
+			oos.writeObject(block);
+			oos.flush();
+			oos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
--- Monty/src/main/java/monty/Main.java
+++ Monty/src/main/java/monty/Main.java
@@ -16,93 +16,24 @@ limitations under the License.
 
 package monty;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.List;
-import ast.Block;
-import lexer.LexerConfig;
-import lexer.MontyToken;
-import parser.MontyException;
-import parser.parsing.Parser;
-
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 public class Main {
 	public static String[] argv = null;
 	public static String path;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		if (args.length == 0 || args[0] == "-h") {
-			System.out.println("To run:\tjava -jar Monty.jar [file_name.(mt|mtc)]");
-			System.out.println("To compile:\tjava -jar Monty.jar [input_file_name.mt] -o [output_file_name.mtc]");
-
-		} else {
+		if (args.length == 0 || args[0] == "-h")
+			System.out.println("To run:\tjava -jar Monty.jar [file_name.(mt|mtc)]\nTo compile:\\tjava -jar Monty.jar [input_file_name.mt] -o [output_file_name.mtc]");
+		else {
 			argv = args;
-
-			// path = Main.class.getResource("Examples/files.mt").getPath();
-			path = args[0];
-
 			int from = argv.length > 1 && argv[1].equals("-o") ? 3 : 1;
-			var block = getBlock(from);
+			var block = IOBlocks.readBlock(from, args[0]);
 			if (from == 1)
 				block.run();
-			else {
-				FileOutputStream fos = new FileOutputStream(argv[2]);
-				GZIPOutputStream gos = null;
-				try {
-					gos = new GZIPOutputStream(fos);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-
-				try {
-					ObjectOutputStream oos = new ObjectOutputStream(gos);
-					oos.writeObject(block);
-					oos.flush();
-					oos.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+			else 
+				IOBlocks.compileAndWriteBlock(block, argv[2]);
 		}
 	}
 
-	public static Block getBlock(int from) {
-		Block block = null;
-		Importing.setLibraries(from);
-		if (path.endsWith(".mt")) {
-			var lb = LexerConfig.getLexer(FileIO.readFile(path));
-
-			List<MontyToken> tokens = lb.getAllTokens();
-			block = Parser.parse(tokens);
-			block.addFunction(new sml.data.returning.Nothing());
-		} else if (path.endsWith(".mtc")) {
-			FileInputStream fis = null;
-			GZIPInputStream gis = null;
-			try {
-				fis = new FileInputStream(path);
-				gis = new GZIPInputStream(fis);
-			} catch (IOException e) {
-				new MontyException("File not found:\t" + path);
-			}
-
-			ObjectInputStream ois;
-			try {
-				ois = new ObjectInputStream(gis);
-				block = (Block) ois.readObject();
-				ois.close();
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-				new MontyException("Wrong file format");
-			}
-		} else {
-			new MontyException("Wrong file extensions");
-		}
-		return block;
-	}
 }
