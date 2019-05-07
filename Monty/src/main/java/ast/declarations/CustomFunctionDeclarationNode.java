25e6d79f52db1666e4fc2226edcc73f510eb191d
Frajerzycki <32599796+frajerzycki@users.noreply.github.com>
2019-05-07 19:03:53 +0200

    Doesn't implement setArguments.

--- Monty/src/main/java/ast/declarations/CustomFunctionDeclarationNode.java
+++ Monty/src/main/java/ast/declarations/CustomFunctionDeclarationNode.java
@@ -65,32 +65,6 @@ public final class CustomFunctionDeclarationNode extends FunctionDeclarationNode
         copied.body = body.copy();
         return copied;
     }
-    protected void setArguments(Tuple arguments, String callFileName, int callLine) {
-        var argumentsLength = arguments.length();
-        if (parameters.length == 1 && parameters.length  != argumentsLength) {
-            var name = this.parameters[0];
-            VariableDeclarationNode variable;
-            if (!body.hasVariable(name)) {
-                variable = new VariableDeclarationNode(name);
-                body.addVariable(variable, getFileName(), getLine());
-            } else
-                variable = body.getVariable(name, getFileName(), getLine());
-            variable.setValue(arguments, callFileName,callLine);
-            variable.setConst(Character.isUpperCase(name.charAt(0)));
-            return;
-        } else if (argumentsLength > parameters.length)
-            new LogError("Too many arguments in " + name + " function call.", callFileName, callLine);
-        else if (argumentsLength < parameters.length)
-            new LogError("Too few arguments in " + name + " function call.", callFileName, callLine);
-
-        for (int i = 0; i < arguments.length(); i++) {
-            var name = parameters[i];
-            VariableDeclarationNode variable = new VariableDeclarationNode(name);
-            variable.setValue(arguments.get(i), callFileName, callLine);
-            variable.setConst(Character.isUpperCase(name.charAt(0)));
-            body.addVariable(variable, callFileName, callLine);
-        }
-    }
 
     @Override
     protected void setArgument(Object value, int index, String fileName, int line) {
